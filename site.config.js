b916fccf61738f7764c1d076e38b838e72aee101
Caissonpoint <122631730+caissonpoint@users.noreply.github.com>
2023-11-20 15:09:35 +0000

    ok

--- site.config.js
+++ site.config.js
@@ -2,11 +2,11 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Eric B",
-    // image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-   // role: "frontend developer",
-    // bio: "I develop everything using node.",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+   role: "frontend developer",
+    bio: "I develop everything using node.",
     email: "eb@caissonpoint.com",
-   //  linkedin: "morethanmin",
+    linkedin: "morethanmin",
     github: "caissonpoint",
     instagram: "ovrnightr",
   },
